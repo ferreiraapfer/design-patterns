59ebb8b1a3a3de2d8cd6c0d20b8a6134ea5209d6
Fernanda Aparecida Ferreira <fernanda.ferreira@tegra.com.br>
2019-04-16 17:32:20 -0300

    Aula 02 - Design Patters

--- src/Investimentos/Conta.java
+++ src/Investimentos/Conta.java
@@ -2,12 +2,18 @@ package Investimentos;
 
 public class Conta {
 
+	private String titular;
 	private double saldo;
-	
-	public void deposita(double valor) {
+
+	public void deposita(String titular, double valor) {
+		this.titular = titular;
 		this.saldo += valor;
 	}
-	
+
+	public String getTitular() {
+		return titular;
+	}
+
 	public double getSaldo() {
 		return saldo;
 	}
--- src/Investimentos/RealizadorDeInvestimentos.java
+++ src/Investimentos/RealizadorDeInvestimentos.java
@@ -5,7 +5,7 @@ public class RealizadorDeInvestimentos {
 	public void realizarInvesvimento(Investimento investimento, Conta conta) {
 		investimento.investe(conta);
 
-		conta.deposita(500);
+		conta.deposita("Joao", 500);
 		System.out.println(conta.getSaldo());
 	}
 }
